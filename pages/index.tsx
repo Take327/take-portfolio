98f6a9fd701aa51f1567f481978655e32de8b087
Take327 <gatewayseed2@gmail.com>
2021-06-25 01:06:38 +0900

    css修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Header from '../components/header/header'
 import SideNavi from '../components/navi/sideNavi'
