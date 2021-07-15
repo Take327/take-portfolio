bd2dbc1ada8df6cb4cba8f2b66ac6bde010dcb9b
Take327 <gatewayseed2@gmail.com>
2021-07-15 23:56:35 +0900

    ボタンCSS修正中

--- components/main/Top/Button/ScrollButton.tsx
+++ components/main/Top/Button/ScrollButton.tsx
@@ -3,11 +3,11 @@ import styles from './ScrollButton.module.scss'
 
 
 
-const ScrollButton = () => {
+const ScrollButton: FC = () => {
 
     return (
         <div className={styles.basicBox}>
-            Hover
+            ポートフォリオを見る
             <svg width="130" height="65" viewBox="0 0 130 65" xmlns="http://www.w3.org/2000/svg">
                 <rect x='0' y='0' fill='none' width='130' height='65' />
             </svg>
