51b3e8597832cdd020359441bdd01bd85777f694
Take327 <gatewayseed2@gmail.com>
2021-07-16 00:10:57 +0900

    CSSボタンにスクロール機能追加

--- components/main/Top/Button/ScrollButton.tsx
+++ components/main/Top/Button/ScrollButton.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react'
 import styles from './ScrollButton.module.scss'
+import {smoothScrollAction} from '../../../util/smoothScrollAction'
+import {naviData} from '../../../../dataset/naviData'
 
 
 
@@ -7,8 +9,8 @@ const ScrollButton = () => {
 
     return (
         <div className={styles.basicBox}>
-            Hover
-            <svg width="130" height="65" viewBox="0 0 130 65" xmlns="http://www.w3.org/2000/svg">
+            CONTACT
+            <svg width="130" height="65" viewBox="0 0 130 65" xmlns="http://www.w3.org/2000/svg" onClick={()=>smoothScrollAction('#contact')}>
                 <rect x='0' y='0' fill='none' width='130' height='65' />
             </svg>
         </div>
